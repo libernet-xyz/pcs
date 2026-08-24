070feb422cf1db19095989d76eb07cb609c01850
71104 <a71104@gmail.com>
2026-08-24 16:01:33 +0200

    Update.

--- src/merkle.rs
+++ src/merkle.rs
@@ -1,4 +1,5 @@
 use crate::hash::{Hasher, MerkleHasher};
+use anyhow::{Result, anyhow};
 use primitive_types::H256;
 use starkom_ff::{Field, Field256};
 use std::marker::PhantomData;
@@ -56,8 +57,209 @@ pub(crate) fn merklify<H: MerkleHasher>(mut hashes: &mut [H256], mut n: usize) {
 /// are reconstructed separately during the verification of a whole `Query`. In particular, all root
 /// hashes are stored in the `Commitment`.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct Proof<F: Field, G: Field256 + From<F>, H: Hasher<G>> {
+pub(crate) struct Proof<F: Field + From<usize>, G: Field256 + From<F>, H: Hasher<G>> {
     leaf: Vec<F>,
     path: Vec<H256>,
     _data: PhantomData<(G, H)>,
+}
+
+impl<F: Field + From<usize>, G: Field256 + From<F>, H: Hasher<G>> Proof<F, G, H> {
+    /// Returns a reference to the leaf values (one for every committed polynomial).
+    pub(crate) fn leaf(&self) -> &[F] {
+        self.leaf.as_slice()
+    }
+
+    /// Checks the leaf of this proof against the provided slice.
+    ///
+    /// The two must match or an error is returned.
+    pub(crate) fn check_leaf(&self, expected: &[F]) -> Result<()> {
+        if expected.len() != self.leaf.len()
+            || self
+                .leaf
+                .iter()
+                .zip(expected.iter())
+                .any(|(&value1, &value2)| value1 != value2)
+        {
+            return Err(anyhow!("leaf value mismatch"));
+        }
+        Ok(())
+    }
+
+    /// Returns the length of the Merkle path, corresponding to the height of the tree minus 1 (the
+    /// root hash is not included in this count).
+    pub(crate) fn len(&self) -> usize {
+        self.path.len()
+    }
+
+    /// Verifies the proof against the given root hash.
+    pub(crate) fn verify(&self, mut index: usize, root_hash: H256) -> Result<()> {
+        let mut hash = hash_leaf::<F, G, H>(self.leaf.as_slice());
+        for &sibling in &self.path {
+            hash = if index & 1 != 0 {
+                H::hash_binary(sibling, hash)
+            } else {
+                H::hash_binary(hash, sibling)
+            };
+            index >>= 1;
+        }
+        if index != 0 {
+            return Err(anyhow!("invalid index"));
+        }
+        if hash != root_hash {
+            return Err(anyhow!(
+                "root hash mismatch (got {}, want {})",
+                hash,
+                root_hash
+            ));
+        }
+        Ok(())
+    }
+
+    /// Returns a boolean indicating whether or not the committed polynomials are constant.
+    ///
+    /// An error is returned if one or more hashes in the proof are out of range; see [`Hash<H256>`]
+    /// for details.
+    ///
+    /// This function is used in low degree testing to check when the folding process collapses to
+    /// degree-0 polynomials.
+    ///
+    /// Note that some polynomials may collapse earlier than others, and this function returns false
+    /// if one or more haven't collapsed yet. So it returns true if and only if all have collapsed.
+    pub(crate) fn is_constant(&self) -> Result<bool> {
+        let mut hash = hash_leaf::<F, G, H>(self.leaf.as_slice());
+        for &sibling in &self.path {
+            if sibling != hash {
+                return Ok(false);
+            }
+            hash = H::hash_binary(hash, hash);
+        }
+        Ok(true)
+    }
+}
+
+// TODO
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::hash::{Keccak256Hash, Sha2Hash};
+    use starkom_bluesky::Scalar as BS;
+
+    fn parse_hash(s: &'static str) -> H256 {
+        s.parse().unwrap()
+    }
+
+    #[test]
+    fn test_merklify_one_sha2() {
+        let mut hashes = vec![parse_hash(
+            "0x1fe9e33b7ff790473e13eb6384d61d6abb32d2e50b30a382c9767b5347f5846b",
+        )];
+        merklify::<Sha2Hash<BS>>(&mut hashes, 1);
+        assert_eq!(
+            hashes,
+            vec![parse_hash(
+                "0x1fe9e33b7ff790473e13eb6384d61d6abb32d2e50b30a382c9767b5347f5846b"
+            )]
+        );
+    }
+
+    #[test]
+    fn test_merklify_one_keccak256() {
+        let mut hashes = vec![parse_hash(
+            "0x1fe9e33b7ff790473e13eb6384d61d6abb32d2e50b30a382c9767b5347f5846b",
+        )];
+        merklify::<Keccak256Hash<BS>>(&mut hashes, 1);
+        assert_eq!(
+            hashes,
+            vec![parse_hash(
+                "0x1fe9e33b7ff790473e13eb6384d61d6abb32d2e50b30a382c9767b5347f5846b"
+            )]
+        );
+    }
+
+    #[test]
+    fn test_merklify_two_sha2() {
+        let mut hashes = vec![
+            parse_hash("0x0cbb1061e55efef40fae3c8e34d301c9940889146f816edd475052fc45caf060"),
+            parse_hash("0x2d8257d72c4b6e6bfa3a21a22c053d02d1d52b4b5e6097acfd0ed45f827d6ba4"),
+        ];
+        hashes.resize(3, H256::default());
+        merklify::<Sha2Hash<BS>>(&mut hashes, 2);
+        assert_eq!(
+            hashes,
+            vec![
+                parse_hash("0x0cbb1061e55efef40fae3c8e34d301c9940889146f816edd475052fc45caf060"),
+                parse_hash("0x2d8257d72c4b6e6bfa3a21a22c053d02d1d52b4b5e6097acfd0ed45f827d6ba4"),
+                parse_hash("0x3af448b6612ac9931b7413ecca5d209187cd808ae2d3647099aa99fada16c955")
+            ]
+        );
+    }
+
+    #[test]
+    fn test_merklify_two_keccak256() {
+        let mut hashes = vec![
+            parse_hash("0x0cbb1061e55efef40fae3c8e34d301c9940889146f816edd475052fc45caf060"),
+            parse_hash("0x2d8257d72c4b6e6bfa3a21a22c053d02d1d52b4b5e6097acfd0ed45f827d6ba4"),
+        ];
+        hashes.resize(3, H256::default());
+        merklify::<Keccak256Hash<BS>>(&mut hashes, 2);
+        assert_eq!(
+            hashes,
+            vec![
+                parse_hash("0x0cbb1061e55efef40fae3c8e34d301c9940889146f816edd475052fc45caf060"),
+                parse_hash("0x2d8257d72c4b6e6bfa3a21a22c053d02d1d52b4b5e6097acfd0ed45f827d6ba4"),
+                parse_hash("0x6e70b19bd160e181bb367ab730eababc6e3673977f7fdce38934ee197bbcb568")
+            ]
+        );
+    }
+
+    #[test]
+    fn test_merklify_four_sha2() {
+        let mut hashes = vec![
+            parse_hash("0x034acf2cced8e9744784ec9c3c626fa83f6f8ddd83faed9d5caa5ad72c91eb1c"),
+            parse_hash("0x39dce95a2271a57f999981eef6917f3df8ad25d116c98d809a3b7da5d54805a3"),
+            parse_hash("0x435246f701f1483adcb7037fa64b3fa027c41e13a52d1e6c502e9b71dea3ca81"),
+            parse_hash("0x7952ed41c4062f594b53b8c548874b41a7a7f05593d7a5a313ed82c2fe1c62d7"),
+        ];
+        hashes.resize(7, H256::default());
+        merklify::<Sha2Hash<BS>>(&mut hashes, 4);
+        assert_eq!(
+            hashes,
+            vec![
+                parse_hash("0x034acf2cced8e9744784ec9c3c626fa83f6f8ddd83faed9d5caa5ad72c91eb1c"),
+                parse_hash("0x39dce95a2271a57f999981eef6917f3df8ad25d116c98d809a3b7da5d54805a3"),
+                parse_hash("0x435246f701f1483adcb7037fa64b3fa027c41e13a52d1e6c502e9b71dea3ca81"),
+                parse_hash("0x7952ed41c4062f594b53b8c548874b41a7a7f05593d7a5a313ed82c2fe1c62d7"),
+                parse_hash("0x3d139c65859969e86db444ccc0d36ffd0456b3f92d3dfb34fa10c8c07f5ada06"),
+                parse_hash("0xdc3dd313db4b53f2720fcfe6373d286a9c40911546e1c434fa24ab650a75b586"),
+                parse_hash("0xed171c6bd6876b93f4fc8914e0b2a8db53d7204048ad9fdff6e7127c077ca072"),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_merklify_four_keccak256() {
+        let mut hashes = vec![
+            parse_hash("0x034acf2cced8e9744784ec9c3c626fa83f6f8ddd83faed9d5caa5ad72c91eb1c"),
+            parse_hash("0x39dce95a2271a57f999981eef6917f3df8ad25d116c98d809a3b7da5d54805a3"),
+            parse_hash("0x435246f701f1483adcb7037fa64b3fa027c41e13a52d1e6c502e9b71dea3ca81"),
+            parse_hash("0x7952ed41c4062f594b53b8c548874b41a7a7f05593d7a5a313ed82c2fe1c62d7"),
+        ];
+        hashes.resize(7, H256::default());
+        merklify::<Keccak256Hash<BS>>(&mut hashes, 4);
+        assert_eq!(
+            hashes,
+            vec![
+                parse_hash("0x034acf2cced8e9744784ec9c3c626fa83f6f8ddd83faed9d5caa5ad72c91eb1c"),
+                parse_hash("0x39dce95a2271a57f999981eef6917f3df8ad25d116c98d809a3b7da5d54805a3"),
+                parse_hash("0x435246f701f1483adcb7037fa64b3fa027c41e13a52d1e6c502e9b71dea3ca81"),
+                parse_hash("0x7952ed41c4062f594b53b8c548874b41a7a7f05593d7a5a313ed82c2fe1c62d7"),
+                parse_hash("0x8a2dbe0367d89a172a0eb470026f5bb85ddd542e0d93885318c5d6b8a3764f28"),
+                parse_hash("0x31354bf6d6ca07dbde80395609890a558ff35587ddc2b5c72ccde2b4e15d114f"),
+                parse_hash("0xbd82b7f4f83de57b73e00aa0924a5fc4dfaf959bd252aa3fda8abdaf87713dc0"),
+            ]
+        );
+    }
+
+    // TODO
 }
